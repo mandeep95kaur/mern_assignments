0926ee26578cde3c2a24738ad2a8cdadca054631
unknown <saini1995mandeep@gmail.com>
2021-09-20 14:05:22 -0700

    todo-list1

--- mern_assignments/todo-list/src/components/Todo.js
+++ mern_assignments/todo-list/src/components/Todo.js
@@ -1,14 +1,30 @@
-const Todo= (props) => {
-    return(
-        <div>
-            <input onChange={(event) =>{
-              props.handleToggleComplete(props.i);
-            }} checked={props.todo.complete} type="checkbox" />
-            <span>{props.todo.text}</span>
-            <button onClick={(event) =>{
-              props.handleTodoDelete(props.i);
-            }} style={{ marginLeft : "20px"}}>Delete</button>
-          </div>
-    )
-}
+const Todo = (props) => {
+  return (
+    <div>
+      <input
+        onChange={(event) => {
+          props.handleToggleComplete(props.i);
+        }}
+        checked={props.todo.complete}
+        type="checkbox"
+      />
+      {props.todo.complete ? (
+        <span style={{ textDecoration: "line-through" }}>
+          {props.todo.text}
+        </span>
+      ): (
+        <span>{props.todo.text}</span>
+      )}
+
+      <button
+        onClick={(event) => {
+          props.handleTodoDelete(props.i);
+        }}
+        style={{ marginLeft: "20px" }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 export default Todo;
