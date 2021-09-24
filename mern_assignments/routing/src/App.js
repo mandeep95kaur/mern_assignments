e89294f70df1ef67f7432944de854fb1ac7c8156
unknown <saini1995mandeep@gmail.com>
2021-09-24 15:52:03 -0700

    Routing Assignment

---
+++ mern_assignments/routing/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import React from "react";
+import { Router } from "@reach/router";
+import Home from "./components/Home";
+import Display1 from "./components/Display1";
+import DisplayWordColor from "./components/DisplayWordColor";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Home path="/home" />
+        <Display1 path="/:intOrWord" />
+        <DisplayWordColor path=":word/:fontColor/:backgroundColor" />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ mern_assignments/routing/src/components/Display1.js
@@ -0,0 +1,7 @@
+import { Router } from "@reach/router";
+import React from "react";
+const Display1 = (props) => {
+  return <h1>{props.intOrWord}</h1>;
+};
+
+export default Display1;
